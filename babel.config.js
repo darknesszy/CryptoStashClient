8c37693b6e4620a99b62bf63ea1b9d0ac77060ac
Michael <25147960+darknesszy@user.noreply.github.com>
2021-11-01 15:40:11 +0800

    :sparkles: Added tab and drawer navigation

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    // CHANGE: react-native-reanimated
+    'react-native-reanimated/plugin'
+    // END CHANGE: react-native-reanimated
+  ]
 };
--- src/navigation.js
+++ src/navigation.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import { TestScreen1, TestScreen2 } from './Screens/TestScreen'
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator()
+const Tab = createBottomTabNavigator()
+const Test = createNativeStackNavigator()
+
+const TestStack = () => (
+    <Test.Navigator>
+        <Test.Screen name="Test1" component={TestScreen1} />
+        <Test.Screen name="Test2" component={TestScreen2} />
+    </Test.Navigator>
+)
+
+const TabNavigation = () => (
+    <Tab.Navigator>
+        <Tab.Screen name="Test Stack" component={TestStack} />
+    </Tab.Navigator>
+)
 
 export default Navigation = () => (
     <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen name="Test1" component={TestScreen1} />
-            <Stack.Screen name="Test2" component={TestScreen2} />
-        </Stack.Navigator>
+        <Drawer.Navigator>
+            <Drawer.Screen name="Tabs" component={TabNavigation} />
+        </Drawer.Navigator>
     </NavigationContainer>
 )
